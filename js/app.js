bc98524b6a51ae33b1159c0fc16256cfcc93d1ac
Olga <olyavolk49@gmail.com>
2020-12-21 20:54:13 +0200

    добавила объект с приложениями и модули

--- js/app.js
+++ js/app.js
@@ -1,21 +1,39 @@
 
+import Like from './modules/Like.js';
+
+
+
 let wrapper = document.querySelector('.wrapper');
-console.log(wrapper);
 let sideButton = document.querySelector('.side-button');
-console.log(sideButton);
-// let menuButton = document.querySelector('.menu-button');
-// console.log(menuButton);
+let menuButton = document.querySelector('.menu-button');
+
+
+
+let apps = { // applications
+    like_app: Like
+}
+
+console.log(apps);
+
+
+
+
+
+
+
 
 
 let enabled = false;
 
 sideButton.addEventListener('click', toogleDisplay);
+menuButton.addEventListener('click', toogleDisplay);
 
 
 function toogleDisplay() {
     enabled = !enabled;
     if (enabled) {
         enableDisplay()
+
     } else {
         disableDisplay();
     }
@@ -29,12 +47,27 @@ function enableDisplay() {
     const html = buildMenu();
     display.innerHTML = html;
 
+
     console.log('показываю экран');
 }
 
 function buildMenu() {
-    return '<div class="menu">'
-        + '<div class="menu-item"> 123 </div>'
+
+    let applications = Object.keys(apps);
+
+    let content = "";
+
+    applications.map(app_key => { // app_key = like_app
+        let app = apps[app_key]
+        content +=
+            "<div class='" + app.icon + "' onclick='"
+            + app.program + "'>"
+            + app.name +
+            "</div>"
+    });
+
+    return '<div class="menu-container">'
+        + content
         + '</div>';
 }
 
---
+++ js/modules/Like.js
@@ -0,0 +1,7 @@
+export default {
+    name: "Like",
+    icon: "far fa-heart",
+    program: function () {
+        alert("Like")
+    }
+}
